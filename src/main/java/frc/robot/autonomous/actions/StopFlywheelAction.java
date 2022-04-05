fda53cd82342f198e715f1b7715bc01a11f3e27d
Philip Leal <philleal@gmail.com>
2022-04-05 15:40:35 -0500

    Added the StopFlywheelAction class

---
+++ src/main/java/frc/robot/autonomous/actions/StopFlywheelAction.java
@@ -0,0 +1,34 @@
+package frc.robot.autonomous.actions;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Robot;
+import frc.robot.subsystems.FlyWheel;
+
+public class StopFlywheelAction extends CommandBase {
+    public StopFlywheelAction() {
+        addRequirements(Robot.flyWheel);
+    }
+
+    public void initialize() {
+        
+    }
+
+    public void execute() {
+        Robot.flyWheel.stop(false);
+    }
+
+    @Override
+    public boolean isFinished() {
+        //Robot.flyWheel.stop(false);
+        return true;
+    }
+
+    protected void end() {
+        //Robot.flyWheel.stop(false);
+    }
+
+    protected void interrupted() {
+        end();
+    }
+}
